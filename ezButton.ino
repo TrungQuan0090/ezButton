3fd51d85d0bbc5999010e23b7186466426b2ff80
Trung Quan <0308221160@caothang.edu.vn>
2024-06-11 12:21:39 +0700

    Create ezButton.ino

---
+++ ezButton.ino
@@ -0,0 +1,22 @@
+#include <ezButton.h>
+
+ezButton button(7);  // create ezButton object that attach to pin 7;
+
+void setup() {
+  Serial.begin(9600);
+  button.setDebounceTime(100); // set debounce time to 100 milliseconds
+ 
+  button.setCountMode(COUNT_FALLING);
+}
+
+void loop() {
+  button.loop(); // MUST call the loop() function first
+
+  unsigned long count = button.getCount();
+  Serial.println(count);
+
+  if(count >= 100)
+    button.resetCount();
+
+  
+}
